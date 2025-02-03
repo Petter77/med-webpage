b521b059cdf5f4185bc7f16f7214f9903e85c6d0
ExoProx <dnawojczyk@wp.pl>
2025-02-03 02:22:28 +0100

    Update insert_data_wyniki.php

--- insert_data_wyniki.php
+++ insert_data_wyniki.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idPersonelu = $_SESSION['id'];
     $wynikiBadania = $_POST['examinationDetails'];
     $dataWyniku = $_POST['examinationDate'];
-    $today = date('D-M-Y');
+    $today = date('Y-m-d);
     if ($dataWyniku > $today) {
         if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], "https://studencki-portal-medyczny.pl/techinical_panel.php") !== false) {
         echo '<script type="text/javascript">
