3d84384891f481e5f470b37c70fbac8b38a28cf5
ExoProx <dnawojczyk@wp.pl>
2025-02-03 01:10:28 +0100

    Poprawka kolejna

--- js/script.js
+++ js/script.js
@@ -434,18 +434,21 @@ document.addEventListener('DOMContentLoaded', function () {
             `;
         }
         else if (event.target && event.target.id === 'addPapersButton') {
-            const today = new Date().toISOString().split('T')[0];
+            const this = new Date();
+            const formattedToday = this.toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+
+            // Populate the form with today's date, restricting future dates
             document.getElementById('elementDetails').innerHTML = `
-                <h2>Dodaj nowe Wyniki</h2>
-                <form action="insert_data_wyniki.php" method="post" enctype="multipart/form-data">
-                    <label for="elementName">Wyniki Badania:</label>
-                    <textarea id="elementDetailsTextarea" name="examinationDetails" maxlength="256"></textarea>
-                    <label for="elementDetailsTextarea">Data Przeprowadzenia Wyników:</label>
-                    <input type="date" id="elementDetailsTextarea" name="examinationDate" value="${today}" max="${today}">
-                    <label for="fileInput">Załącz plik:</label>
-                    <input type="file" id="fileInput" name="plik" accept=".jpg,.jpeg,.png,.pdf">
-                    <button type="submit" class="button">Dodaj</button>
-                </form>
+        <h2>Dodaj nowe Wyniki</h2>
+        <form action="insert_data_wyniki.php" method="post" enctype="multipart/form-data">
+            <label for="elementName">Wyniki Badania:</label>
+            <textarea id="elementDetailsTextarea" name="examinationDetails" maxlength="256"></textarea>
+            <label for="elementDetailsTextarea">Data Przeprowadzenia Wyników:</label>
+            <input type="date" id="elementDetailsTextarea" name="examinationDate" value="${formattedToday}" max="${formattedToday}">
+            <label for="fileInput">Załącz plik:</label>
+            <input type="file" id="fileInput" name="plik" accept=".jpg,.jpeg,.png,.pdf">
+            <button type="submit" class="button">Dodaj</button>
+        </form>
             `;
         }
         else if(event.target && event.target.id === 'addAllergiesButton'){
